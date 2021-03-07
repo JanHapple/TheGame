8ee75e48edbb026f2cf3eeb2128f114a27a63a4f
FloH <mail@FlorianHoehle.de>
2021-03-07 19:17:54 +0100

    bag implemented

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var currScene, option;
+var currScene, option, bagStatus = 0, bagItem;
+var bagContent = ["no items"];
 var dungeonOptions = ["a pile of ruins.", "a dirty sheet.", "the door.", "Azruk."];
 
 
@@ -9,19 +10,53 @@ function checkKey(k) {
     k = k || window.event;
 
     if(k.keyCode == "38"){
-        // up arrow
+    // up arrow
         window["scene"][currScene](10);
     }else if(k.keyCode == "37"){
-        // left arrow
-       window["scene"][currScene](-1);
+    // left arrow
+        if(bagStatus == 1){
+            bag(-1);
+        }else{
+            window["scene"][currScene](-1);
+        }
     }else if(k.keyCode == "39"){
-        // right arrow
-       window["scene"][currScene](+1);
-    }else if(k.keyCode == "66"){
-        // B key
-        alert("bag opened");
+    // right arrow
+        if(bagStatus == 1){
+            bag(1);
+        }else{
+            window["scene"][currScene](1);
+        }
+    }else if(k.keyCode == "40"){
+    // down arrow
+        if(bagStatus == 0){
+            bagStatus = 1;
+            document.getElementById("bagStatus").innerHTML = "Bag: ";
+            document.getElementById("bag").innerHTML = bagContent[0];
+        }else if(bagStatus == 1){
+            bagStatus = 0;
+            document.getElementById("bagStatus").innerHTML = "";
+            document.getElementById("bag").innerHTML = "";
+        }
     }
 }
+
+
+// bag
+function bag(c){
+   
+    bagItem += c;
+
+    // cycle
+    if(bagItem > (bagContent.length - 1)){
+        bagItem -= bagContent.length;
+    }else if(bagItem < 0){
+        bagItem = bagContent.length - 1;
+    }
+
+    // print option
+    return document.getElementById("bag").innerHTML = bagContent[bagItem];
+}
+
 
 // scenes
 var scene = {
@@ -38,7 +73,7 @@ var scene = {
             // interact
                 if(option == 1){
                 // take sheet from tunnel entrance
-                    dungeonOptions[1] = "tunnel";
+                    dungeonOptions[1] = "a tunnel.";
                 }
                 break;
             default:
